00a711809cd883e206fcea19ad3bdf044a8a4219
Krishnanunni R <krishnanunnir97@gmail.com>
2024-09-22 17:07:28 +0530

    increase time

--- utils.py
+++ utils.py
@@ -182,7 +182,7 @@ def main():
         scraper.quit()
 
 
-schedule.every(1).minutes.do(main)
+schedule.every(10).minutes.do(main)
 
 git_hash, commit_message = get_git_info()
 logger.info(f"Current Git hash: {git_hash}")
